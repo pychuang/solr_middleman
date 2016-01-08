774dc4039925fdd8ff1beb901a9f8f52f9de3196
Po-Yu Chuang <ratbert.chuang@gmail.com>
2016-01-08 18:54:35 -0500

    select_handler/urls.py

--- solr_middleman/urls.py
+++ solr_middleman/urls.py
@@ -6,6 +6,7 @@ urlpatterns = [
     # url(r'^$', 'solr_middleman.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
+    url(r'^select/', include('select_handler.urls', namespace='select_handler')),
     url(r'^admin/', include(admin.site.urls)),
     url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework'))
 ]
