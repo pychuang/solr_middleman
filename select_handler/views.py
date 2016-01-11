f9fbbd077bb0331df65bac5373e431b0bc43d479
Po-Yu Chuang <ratbert.chuang@gmail.com>
2016-01-10 21:34:04 -0500

    QuerySerializer

--- select_handler/views.py
+++ select_handler/views.py
@@ -3,10 +3,12 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 
 from .models import Query
+from .serializers import QuerySerializer
 
 class QueryView(APIView):
     def get(self, request, format=None):
         q = request.GET.get('q', None)
         print 'QUERY:', q
-        query = Query(q)
-        return Response(q)
+        query = Query(q=q)
+        serializer = QuerySerializer(query)
+        return Response(serializer.data)
