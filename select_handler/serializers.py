50879981a22bb640ae526016877aa246b9676ae0
Po-Yu Chuang <ratbert.chuang@gmail.com>
2016-01-13 00:40:47 -0500

    WIP

--- select_handler/serializers.py
+++ select_handler/serializers.py
@@ -7,3 +7,23 @@ class QuerySerializer(serializers.ModelSerializer):
    class Meta:
         model = Query
         fields = ('q',)
+
+
+class SolrResultSerializer(serializers.Serializer):
+#    id = serializers.CharField(max_length=255)
+    doi = serializers.CharField(max_length=255)
+#    incol = serializers.BooleanField()
+#    title = serializers.CharField(max_length=255)
+#    venue = serializers.CharField(max_length=255)
+#    abstract = serializers.TextField()
+#    ncites = serializers.IntegerField()
+#    scites = serializers.IntegerField()
+#    year = serializers.IntegerField()
+    author = serializers.ListField(
+        child = serializers.CharField(max_length=255)
+    )
+#    vtime = serializers.IntegerField()
+
+    def __init__(self, **kwargs):
+        self.doi = kwargs['doi']
+        #self.author = kwargs['author']
--- select_handler/views.py
+++ select_handler/views.py
@@ -4,11 +4,13 @@ from rest_framework.response import Response
 
 from .models import Query
 from .serializers import QuerySerializer
+from .serializers import SolrResultSerializer
 
 class QueryView(APIView):
     def get(self, request, format=None):
         q = request.GET.get('q', None)
         print 'QUERY:', q
-        query = Query(q=q)
-        serializer = QuerySerializer(query)
+#        query = Query(q=q)
+#        serializer = QuerySerializer(query)
+        serializer = SolrResultSerializer(doi=q, author=['aa','bb'])
         return Response(serializer.data)
